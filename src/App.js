5e76ad566445f94b73bfbb12d299d28598016104
Kakashidota <robin.kamo@live.se>
2021-05-31 13:10:53 +0200

    testing new deplioy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>potatot</code> and save to reload.
+          Edit <code>Testing agaign</code> and save to reload.
         </p>
         <a
           className="App-link"
